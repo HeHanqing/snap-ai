b88ca3c15f73f078cf528b44b0f35ba3e8a47731
HeHanqing <871026514@qq.com>
2024-04-18 18:30:57 +0800

    fix bug

--- app/api/predictions/[id]/route.ts
+++ app/api/predictions/[id]/route.ts
@@ -4,7 +4,10 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   const prediction = await replicate.predictions.get(params.id);
 
   if (prediction?.error) {
